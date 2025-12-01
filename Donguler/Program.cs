ee809f4e25f21dad405c87c63dd0e0e8a2b3e3d7
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2025-12-02 02:00:02 +0300

    Donguler Add

---
+++ Donguler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loop to print numbers from 1 to 10
+            Console.Write("Lütfen Bir Sayı Giriniz:");
+            int sayac = int.Parse(Console.ReadLine());
+
+            int tekTpolam = 0;
+            int ciftToplam = 0;
+            for (int i = 1; i <= sayac; i++)
+            {
+              if (i % 2 == 1)
+               {
+                  Console.WriteLine($"Tek sayıların Tpolamı : {tekTpolam += i}");
+               }
+        else
+        {
+                  Console.WriteLine($"Çift Sayıların Toplamı:{ciftToplam += i}");
+        }
+            }
+        }
+    }
+}
